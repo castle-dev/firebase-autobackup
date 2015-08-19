4b5da1eee106c76703b14495196d1da0702c2bc4
Scott Lowe <me@scottdlowe.com>
2015-08-19 13:54:52 -0400

    fix(firebase-autobackup.js): limit scope of date variable
    Otherwise, the backups just overwrite each other

--- firebase-autobackup.js
+++ firebase-autobackup.js
@@ -2,25 +2,29 @@ var fs = require('fs');
 var Firebase = require('firebase');
 var ref = new Firebase(process.env.FIREBASE_URL);
 var firstLoad = true;
-var now = new Date();
+var millisecondsBetweenBackups = 14400000; // every four hours
+
+console.log('Starting backup process...');
 
 /* Authenticate to Firebase */
 ref.authWithCustomToken(process.env.FIREBASE_SECRET, function (err) {
   if (err) {
-    console.log(now.toString(), 'Firebase authentication failed!', err);
+    console.log('Firebase authentication failed!', err);
   } else if (firstLoad) {
+    console.log('Firebase authentication successful...');
     setInterval(function () {
+      var now = new Date();
       console.log(now.toString(), 'Backing up the data...');
       ref.once('value', function (snapshot) {
-        var filename = now.toString() + '.json';
+        var filename = now.getTime() + '-' + now.toString() + '.json';
         fs.writeFile('backups/' + filename, JSON.stringify(snapshot.exportVal()), function (err) {
           if (err) { console.log(now.toString(), 'Couldn\'t write to the backup file', err); }
           else { console.log(now.toString(), 'Backup saved!', filename); }
         });
       });
-    }, 14400000); // every 4 hours
+    }, millisecondsBetweenBackups);
   } else {
-      console.log(now.toString(), 'Re-authenticating to firebase');
+      console.log('Re-authenticating to firebase');
   }
 });
 
